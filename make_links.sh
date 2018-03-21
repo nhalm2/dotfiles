616f6e0327bc3e52b9e5a40dbac1e536342d2475
Nick Halm <nhalm@debxps>
2018-03-21 09:05:12 -0500

    Adding to delete the existing symlinks if they exist.  That way this file can be exectued many times.

--- make_links.sh
+++ make_links.sh
@@ -2,7 +2,7 @@
 
 
 echo "making the necesary symlinks"
-echo 
+
 echo
 echo "making .vim symlink"
 if [ -d $HOME/.vim ]; then
@@ -13,19 +13,32 @@ ln -s $(pwd)/vim $HOME/.vim
 
 echo
 echo "making .vimrc symlink"
+if [ -e $HOME/.vimrc ]; then
+	echo "removing existing .vimrc symlink"
+	rm $HOME/.vimrc
+fi
 ln -s $(pwd)/vimrc $HOME/.vimrc
 
 echo
 echo "making .tmux.conf symlink"
+if [ -e $HOME/.tmux.conf ]; then
+	echo "removing existing .tmux.conf symlink"
+	rm $HOME/.tmux.conf
+fi
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 
 echo 
 echo "adding bash functions and aliases"
+if [ -e $HOME/.bash_aliases ]; then
+	echo "removing existing .bash_aliases symlink"
+	rm $HOME/.bash_aliases
+fi
 ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
 
 echo 
 echo "making .gitconfig symlink"
 if [ -e $HOME/.gitconfig ]; then
+	echo "removing existing .gitconfig symlink"
 	rm $HOME/.gitconfig
 fi
 ln -s $(pwd)/gitconfig $HOME/.gitconfig
